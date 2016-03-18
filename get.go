89bbde1983c5fd0bd9bedb1c08506a5387e69c3c
Nicholas Nezis <nicholas.nezis@gmail.com>
2016-03-18 00:25:48 -0400

    Switched Mutex to RWMutex along with some extra Lock/Unlock commands.

--- get.go
+++ get.go
@@ -1,6 +1,8 @@
 package ttlcache
 
 func (t *TTLCache) Get(key string) interface{} {
+    t.lock.RLock()
+	defer t.lock.RUnlock()
 	if k, ok := t.data[key]; ok {
 		return k.Value
 	}
--- set.go
+++ set.go
@@ -5,12 +5,15 @@ import (
 )
 
 func (t *TTLCache) Set(key string, val interface{}) error {
-	k := &Key{
+    k := &Key{
 		Value: val,
 	}
 	// update key
 	k.updated = time.Now()
 
+    t.lock.Lock()
+	defer t.lock.Unlock()
+	
 	t.data[key] = k
 
 	return nil
